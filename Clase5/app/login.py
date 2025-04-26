ef28296119f24d09352c5ec254d5dbb88b1e86f2
Ricardo Paredes <rpcolman2016@gmail.com>
2025-04-26 13:46:56 -0300

    Clase 5

---
+++ Clase5/app/login.py
@@ -0,0 +1,43 @@
+from flask import Blueprint, request, jsonify
+
+login = Blueprint('login', __name__)
+
+@login.route('/login', methods=['POST'])
+def llamarServicioSet():
+    user = request.json.get('user')
+    password = request.json.get('password')
+
+    codRes, menRes, accion = inicializarVariables(user, password)
+
+    salida = {
+        'codRes': codRes,
+        'menRes': menRes,
+        'usuario': user,
+        'accion': accion
+    }
+    return jsonify(salida)
+
+def inicializarVariables(user, password):
+    userLocal = "dxaballero"
+    passLocal = "unida123"
+    codRes = 'SIN_ERROR'
+    menRes = 'OK'
+
+    try:
+        print("Verificar login")
+        if password == passLocal and user == userLocal:
+            print("Usuario y contraseña OK")
+            accion = "Success"
+        else:
+            print("Usuario o contraseña incorrecta")
+            accion = "Usuario o contraseña incorrecta"
+            codRes = 'ERROR'
+            menRes = 'Credenciales o usuario incorrectas'
+
+
+    except Exception as e:
+        print("ERROR", str(e))
+        codRes = 'ERROR'
+        menRes = 'Msg: ' + str(e)
+        accion = "Error interno"
+    return codRes, menRes, accion
