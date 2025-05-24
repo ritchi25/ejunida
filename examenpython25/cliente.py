bf0ebf5b54e1e5bca368b036afd59ed95284e519
RicardoP <rpcolman2016@gmail.com>
2025-05-24 13:17:49 -0300

    intento1

---
+++ examenpython25/cliente.py
@@ -0,0 +1,16 @@
+Entrada = {
+    "ci":"4133266"
+}
+Salida = {
+"accion":"Success",
+"codRes":"SIN ERROR",
+"memRes":"OK",    
+"ci":"4133266"    
+}
+#Si no existe el cliente debe de mostrar:
+{
+ "accion":"Cliente no está en el sistema",
+ "codRes":"ERROR",
+ "memRes":"Error cliente",
+ "ci":"4133267"
+}
---
+++ examenpython25/main.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+from flask import Flask
+
+app = Flask(__name__)
+
+##servicios rest
+
+@app.route('/', methods=['GET'])
+def hello():
+    return 'Hola Mundo'
+
+
+if __name__ == "__main__":
+    
+    app.run(host = '0.0.0.0', debug = True, port = 5003)
+    app.run(debug = True)
